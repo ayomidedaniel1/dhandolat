e2b9a9cfab9442eb6b85a18a07b03d321bcd2004
Ayomide <ayomidedans@gmail.com>
2024-02-03 22:21:06 +0100

    feat: designed hero component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,47 +7,49 @@ const Footer = () => {
 
   return (
     <div className='section__padding bg-[#2C1051]'>
-      <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center px-2 lg:px-20 pb-14 lg:pb-24 lg:pt-7">
-        <h1 className="uppercase text-[#FFC107] font-light text-3xl lg:text-[40px] font-roboto cursor-pointer">logo</h1>
+      <div className="xl:container xl:mx-auto ">
+        <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center px-2 lg:px-20 pb-14 lg:pb-24 lg:pt-7">
+          <h1 className="uppercase text-[#FFC107] font-light text-3xl lg:text-[40px] font-roboto cursor-pointer">logo</h1>
 
-        <div className="flex flex-col justify-center items-start mt-14 lg:mt-0 gap-5 lg:gap-10">
-          <div className="flex flex-row justify-between items-center gap-8 cursor-pointer">
-            <img src={mail} alt="mail" className='w-7 lg:w-[38.44px] h-7 lg:h-[38.44px]' />
-            <span className="font-roboto font-light text-sm lg:text-base text-white uppercase">mail id</span>
+          <div className="flex flex-col justify-center items-start mt-14 lg:mt-0 gap-5 lg:gap-10">
+            <div className="flex flex-row justify-between items-center gap-8 cursor-pointer">
+              <img src={mail} alt="mail" className='w-7 lg:w-[38.44px] h-7 lg:h-[38.44px]' />
+              <span className="font-roboto font-light text-sm lg:text-base text-white uppercase">mail id</span>
+            </div>
+            <div className="flex flex-row justify-between items-center gap-8 cursor-pointer">
+              <img src={call} alt="call" className='w-7 lg:w-[38.44px] h-7 lg:h-[38.44px]' />
+              <span className="font-roboto font-light text-sm lg:text-base text-white">Contact no</span>
+            </div>
           </div>
-          <div className="flex flex-row justify-between items-center gap-8 cursor-pointer">
-            <img src={call} alt="call" className='w-7 lg:w-[38.44px] h-7 lg:h-[38.44px]' />
-            <span className="font-roboto font-light text-sm lg:text-base text-white">Contact no</span>
+
+          <div className="flex flex-col justify-center items-start mt-14 lg:mt-0 gap-7 lg:gap-10">
+            <span className="font-roboto font-light text-sm lg:text-base text-white uppercase cursor-pointer">home</span>
+            <span className="font-roboto font-light text-sm lg:text-base text-white uppercase cursor-pointer">about us</span>
+            <span className="font-roboto font-light text-sm lg:text-base text-white uppercase cursor-pointer">properties</span>
+            <span className="font-roboto font-light text-sm lg:text-base text-white uppercase cursor-pointer">contact</span>
+          </div>
+
+          <div className="flex flex-col justify-center items-start mt-14 lg:mt-0 gap-3">
+            <span className="font-roboto font-light lg:text-2xl text-base text-white uppercase">DIRECTLY CONTACT US ON</span>
+
+            <div className="flex flex-row justify-start items-center whatsapp px-5 py-4 cursor-pointer">
+              <img src={whatsapp} alt="whatsapp" className='w-7 lg:w-[38.44px] h-7 lg:h-[38.44px]' />
+              <span className="font-roboto font-light text-lg lg:text-[32px] text-[#89E4D6] uppercase pl-5 pr-6">whatsapp</span>
+            </div>
           </div>
         </div>
 
-        <div className="flex flex-col justify-center items-start mt-14 lg:mt-0 gap-7 lg:gap-10">
-          <span className="font-roboto font-light text-sm lg:text-base text-white uppercase cursor-pointer">home</span>
-          <span className="font-roboto font-light text-sm lg:text-base text-white uppercase cursor-pointer">about us</span>
-          <span className="font-roboto font-light text-sm lg:text-base text-white uppercase cursor-pointer">properties</span>
-          <span className="font-roboto font-light text-sm lg:text-base text-white uppercase cursor-pointer">contact</span>
+        <div className='-ml-[100000px] w-[100000000px] h-[1px] bg-[#FFFFFF91]' />
+
+        <div className="flex flex-col justify-center items-center mt-10 lg:mt-16 mb-10">
+          <span className="text-white font-light text-sm lg:text-xl text-center">
+            &copy; {currentYear} ALL RIGHTS RESERVED | POWERED BY <span className="text-[#FFC107]"> PROPERTY VISTA</span>
+          </span>
+
+          <span className="text-white font-light text-sm lg:text-xl text-center">
+            DISCLAIMER
+          </span>
         </div>
-
-        <div className="flex flex-col justify-center items-start mt-14 lg:mt-0 gap-3">
-          <span className="font-roboto font-light lg:text-2xl text-base text-white uppercase">DIRECTLY CONTACT US ON</span>
-
-          <div className="flex flex-row justify-start items-center whatsapp px-5 py-4 cursor-pointer">
-            <img src={whatsapp} alt="whatsapp" className='w-7 lg:w-[38.44px] h-7 lg:h-[38.44px]' />
-            <span className="font-roboto font-light text-lg lg:text-[32px] text-[#89E4D6] uppercase pl-5 pr-6">whatsapp</span>
-          </div>
-        </div>
-      </div>
-
-      <div className='-ml-9 lg:-ml-20 w-screen h-[1px] bg-[#FFFFFF91]' />
-
-      <div className="flex flex-col justify-center items-center mt-10 lg:mt-16 mb-10">
-        <span className="text-white font-light text-sm lg:text-xl text-center">
-          &copy; {currentYear} ALL RIGHTS RESERVED | POWERED BY <span className="text-[#FFC107]"> PROPERTY VISTA</span>
-        </span>
-
-        <span className="text-white font-light text-sm lg:text-xl text-center">
-          DISCLAIMER
-        </span>
       </div>
     </div>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import hero from '../assets/hero.svg';
+import hero1 from '../assets/hero1.svg';
+import hero2 from '../assets/hero2.svg';
+import hero3 from '../assets/hero3.svg';
+import hero4 from '../assets/hero4.svg';
 
 const Hero = () => {
   return (
-    <div className='section__padding'>
-      Hero
+    <div className='lg:pl-20 pl-8 pt-16'>
+      <div className=" mask-right xl:container xl:mx-auto lg:pr-12 pb-20 lg:pb-40">
+        <div className="flex flex-col lg:flex-row justify-between items-center">
+          <div className="flex flex-col justify-between items-stretch gap-20">
+            <div className="flex flex-col hero-top justify-center items-center py-20">
+              <h3 className="font-roboto font-medium text-5xl lg:text-[64px] leading-[75px] text-white">DISCOVER</h3>
+              <h3 className="font-roboto font-light text-white text-2xl mt-1 lg:text-[32px] leading-[37px]">Investment Opportunities</h3>
+              <button className="font-roboto font-medium text-lg lg:text-xl text-black bg-[#FFC107] hero-btn mt-7 border-none outline-none">CLICK HERE</button>
+            </div>
+
+            <div className="flex flex-row justify-between items-baseline hero-bottom px-10 lg:px-14 py-4 lg:py-5 gap-12 lg:gap-20">
+              <div className="flex flex-col gap-4 lg:gap-5">
+                <img src={hero1} alt="img" className='w-[27px] h-[37px]' />
+                <span className="font-roboto font-medium text-xs text-white uppercase">growth</span>
+              </div>
+              <div className="flex flex-col gap-4 lg:gap-5">
+                <img src={hero2} alt="img" className='w-[41px] h-[43px]' />
+                <span className="font-roboto font-medium text-xs text-white uppercase">INVESTMENT</span>
+              </div>
+              <div className="flex flex-col gap-4 lg:gap-5">
+                <img src={hero3} alt="img" className='w-[52px] h-[43px]' />
+                <span className="font-roboto font-medium text-xs text-white uppercase">PROPERTY</span>
+              </div>
+              <div className="flex flex-col gap-4 lg:gap-5">
+                <img src={hero4} alt="img" className='w-[41px] h-[49px]' />
+                <span className="font-roboto font-medium text-xs text-white uppercase">ASSISTANCE</span>
+              </div>
+            </div>
+          </div>
+
+          <img src={hero} alt="hero" className='lg:w-[599px] lg:h-[587px]' />
+        </div>
+      </div>
     </div>
   );
 };
