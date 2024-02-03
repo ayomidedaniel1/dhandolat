05d3e745870a9dfc00e485369f4bac566da60878
Ayomide <ayomidedans@gmail.com>
2024-02-03 11:02:43 +0100

    feat: integrated react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Properties from './pages/Properties.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <App />
+    ),
+  },
+  {
+    path: "properties",
+    element: <Properties />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
